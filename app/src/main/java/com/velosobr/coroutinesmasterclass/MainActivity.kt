00252d038b9f1bf6905a326add6a885f1ae4c4b6
velosobr <linoc.veloso@gmail.com>
2025-01-24 15:12:21 -0300

    Initial commit

---
+++ app/src/main/java/com/velosobr/coroutinesmasterclass/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.velosobr.coroutinesmasterclass
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.velosobr.coroutinesmasterclass.ui.theme.CoroutinesMasterClassTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        GlobalScope.launch {
+           repeat(100) {
+               delay(500L)
+               println("Coroutine1 running")
+
+        }
+
+        setContent {
+            CoroutinesMasterClassTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CoroutinesMasterClassTheme {
+        Greeting("Android")
+    }
+}
